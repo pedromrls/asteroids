b128a1eadc7c933f6f3ffedb3509885e2624c0bb
Pedromrls <pedromrls6@gmail.com>
2024-12-16 12:16:14 -0500

    added player logic

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 
 from constants import *
+from player import *
 import pygame  # type: ignore
 
 
 def main():
     pygame.init()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # screen filling
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        # player drawing
+        player.draw(screen)
+
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
