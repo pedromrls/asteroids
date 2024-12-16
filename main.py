69b27b7e5e89ddf9ee6069e9fd98bcd3db88b532
Pedromrls <pedromrls6@gmail.com>
2024-12-16 11:28:31 -0500

    initial game window

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 from constants import *
-import pygame # type: ignore
+import pygame  # type: ignore
+
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+    # print("Starting asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
