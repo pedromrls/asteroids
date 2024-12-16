cd67ea28383d2855a4a7c34142da9b3614ec6924
Pedromrls <pedromrls6@gmail.com>
2024-12-16 11:43:17 -0500

    adding framerate

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import pygame  # type: ignore
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -13,7 +15,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        clock.tick(60)
+        dt =clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
