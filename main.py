e540dc4bdedfd86c3228eec9ccebb5aab2db5928
Pedromrls <pedromrls6@gmail.com>
2024-12-16 11:53:31 -0500

    created the circleshape.py file

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def main():
         screen.fill("black")
         pygame.display.flip()
         clock.tick(60)
-        dt =clock.tick(60)/1000
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
