ec94fdc095861f99b3f3ac851e740228b8736dc9
Pedromrls <pedromrls6@gmail.com>
2024-12-17 13:28:26 -0500

    added the player class to 2 groups

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ def main():
     pygame.init()
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
 
     # screen filling
     while True:
@@ -20,13 +24,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-
-        # player drawing
-        player.draw(screen)
-        player.update(dt)
-        pygame.display.flip()
-        clock.tick(60)
         dt = clock.tick(60) / 1000
+        # player drawing
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
